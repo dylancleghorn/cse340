4137745de9d84e566e3f2db0d8148af8a28a986e
Dylan Cleghorn <dylan.cleghorn@gmail.com>
2026-05-04 16:15:15 -0500

    ejs templating

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import expressLayouts from 'express-ejs-layouts';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -19,20 +20,24 @@ const app = express();
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
 
 /**
  * Routes
  */
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/views/home.html'));
+  res.render('index', { title: 'Home' });
 });
 
 app.get('/organizations', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+  res.render('organizations', { title: 'Organizations' });
 });
 
 app.get('/projects', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/views/projects.html'));
+  res.render('projects', { title: 'Projects' });
 });
 
 app.listen(PORT, () => {
