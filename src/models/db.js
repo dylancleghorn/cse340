0bfb46fbdedf33fc74388c19f8ca1f75c3b94888
Dylan Cleghorn <dylan.cleghorn@gmail.com>
2026-05-05 13:19:43 -0500

    update ssl config

--- src/models/db.js
+++ src/models/db.js
@@ -13,7 +13,7 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL || process.env.DATABASE_URL,
-    ssl: true
+    ssl: { rejectUnauthorized: false }
 });
 
 /**
